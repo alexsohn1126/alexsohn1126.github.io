c27e31f2b1ddd29a0af48097388cb0de0f99192d
Moohaeng Sohn <alexsohn1126@gmail.com>
2024-05-06 22:28:08 -0400

    change sources

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./portfolio-website/src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
